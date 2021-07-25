e5bb6191d06c1414f61913c1401ac2074fccef50
Atsuki Uenishi <cacao3725@gmail.com>
2021-07-25 16:51:06 +0900

    修正

--- routes/api.php
+++ routes/api.php
@@ -29,6 +29,17 @@ Route::group([
     Route::post('logout', [AuthController::class, 'logout']);
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('user', [AuthController::class, 'me']);
+    Route::apiResources([
+        '/v1/users.likes' => LikeController::class, 'getLikes',
+        '/v1/users.reservations' => ReservationController::class, 'getReservations'
+    ]);
+    Route::apiResources([
+        '/v1/likes' => LikeController::class,
+        '/v1/reservations' => ReservationController::class
+    ],
+    [
+        'only' => ['store', 'destroy']
+    ]);
 });
 
 Route::apiResources([
@@ -39,9 +50,11 @@ Route::apiResources([
     'only' => ['index', 'show']
 ]);
 
+
+
 // Route::apiResource('/v1/users', UserController::class);
 // Route::apiResource('/v1/stores', StoreController::class);
-Route::apiResource('/v1/likes', LikeController::class);
-Route::apiResource('/v1/reservations', ReservationController::class);
-Route::get('/v1/users/{user_id}/likes', [LikeController::class, 'getLikes']);
-Route::get('/v1/users/{user_id}/reservations', [ReservationController::class, 'getReservations']);
+// Route::apiResource('/v1/likes', LikeController::class);
+// Route::apiResource('/v1/reservations', ReservationController::class);
+// Route::get('/v1/users/{user_id}/likes', [LikeController::class, 'getLikes']);
+// Route::get('/v1/users/{user_id}/reservations', [ReservationController::class, 'getReservations']);
